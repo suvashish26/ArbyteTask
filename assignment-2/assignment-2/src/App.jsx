e5783bcc862b3b6f4d7abd6e01f2a73cdc3eea0d
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-06-10 14:57:17 +0545

    Latest Commmit

--- assignment-2/assignment-2/src/App.jsx
+++ assignment-2/assignment-2/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+
+  function handleToggleText() {
+    setIsOn(!isOn);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="p-6 text-center">
+      <button
+        onClick={handleToggleText}
+        className=" border rounded-xl px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+      >
+        Turn it {isOn ? "OFF" : "ON"}
+      </button>
+    </div>
   );
 }
 
