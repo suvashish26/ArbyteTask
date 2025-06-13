b767133aec49573c4f8453e3f7edb8e05192afda
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-06-13 22:57:16 +0545

    First Task Added

--- assignment-4/assignment-4/src/App.jsx
+++ assignment-4/assignment-4/src/App.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [changeStyle, setChangeStyle] = useState(false);
+
   useEffect(() => {
     console.log("Component mounted");
-  }, []);
-  const [count, setCount] = useState(0);
+  }, []); // this will run once
 
+  useEffect(() => {
+    console.log("Count changes");
+    if (count === 10) setChangeStyle(true);
+  }, [count]); // maintaining side effect on the count
+
+  console.log(changeStyle);
   function changeCount() {
     setCount(count + 1);
   }
   return (
     <>
       <div>
-        <button onClick={changeCount}>Click Me</button>
+        <button
+          style={{
+            backgroundColor: changeStyle ? "black" : "red",
+            color: changeStyle ? "#fff" : "#000",
+          }}
+          onClick={changeCount}
+        >
+          Click Me
+        </button>
         <p>Increase my {count}</p>
       </div>
     </>
