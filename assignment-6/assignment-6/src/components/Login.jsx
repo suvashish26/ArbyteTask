c489478638252489506438a37963f8dd91d0e2bf
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-06-27 23:13:43 +0545

    feat: add Login component with email and password fields

---
+++ assignment-6/assignment-6/src/components/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <label htmlFor="Email">Email</label>
+      <input type="email" placeholder="Email.." />
+      <label htmlFor="Password">Password</label>
+      <input type="password" placeholder="Password.." />
+    </div>
+  );
+}
