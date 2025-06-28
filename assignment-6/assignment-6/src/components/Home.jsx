1a31d1b20f8a7b64297907a23e352009b07286fc
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-06-28 21:51:09 +0545

    feat: add Home component with links to individual posts

--- assignment-6/assignment-6/src/components/Home.jsx
+++ assignment-6/assignment-6/src/components/Home.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
-        <h1>Welcome To The Page</h1>
+      <h1>Welcome to the Blog</h1>
+      <ul>
+        <li>
+          <Link to="/post/1">Go to Post 1</Link>
+        </li>
+        <li>
+          <Link to="/post/2">Go to Post 2</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
