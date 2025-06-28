96b8ae9b570324798dc08ae26b808264c24864c2
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-06-28 21:53:44 +0545

    feat: set up routing for Home, Login, Register, Message, PostLayout, and PostDetails components

--- assignment-6/assignment-6/src/App.jsx
+++ assignment-6/assignment-6/src/App.jsx
@@ -3,12 +3,18 @@ import Register from "./components/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Message from "./components/Message";
+import PostDetails from "./components/PostDetails";
+import PostLayout from "./components/PostLayout";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
+      <Route path="/message" element={<Message />}></Route>
+      <Route path="/post" element={<PostLayout />} />
+      <Route path=":id" element={<PostDetails />} />
     </Routes>
   );
 }
