ad588b843c5e4c242e790403e7e24f242f3fd4a3
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-06 20:42:48 +0545

    feat: implement protected admin route using isAuthenticated state and ProtectedRoute

--- assignment-6/assignment-6/src/App.jsx
+++ assignment-6/assignment-6/src/App.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Register from "./components/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import PostDetails from "./components/PostDetails";
 import PostLayout from "./components/PostLayout";
-
+import AdminDashboard from "./components/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Login />}></Route>
+      <Route
+        path="/login"
+        element={<Login setIsAuthenticated={setIsAuthenticated} />}
+      />
       <Route path="/register" element={<Register />}></Route>
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/post" element={<PostLayout />}>
         <Route path=":id" element={<PostDetails />} />
       </Route>
