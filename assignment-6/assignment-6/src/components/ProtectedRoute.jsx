884427cbb9c140c0baa79ae22f103bbf361ff50c
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-06 21:55:06 +0545

    feat: create ProtectedRoute component to restrict access by authentication and role

--- assignment-6/assignment-6/src/components/ProtectedRoute.jsx
+++ assignment-6/assignment-6/src/components/ProtectedRoute.jsx
@@ -1,7 +1,15 @@
 import { Navigate } from "react-router";
-export default function ProtectedRoute({ isAuthenticated, children }) {
+export default function ProtectedRoute({
+  isAuthenticated,
+  role,
+  allowedRole,
+  children,
+}) {
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
+  }
+  if (allowedRole && role !== allowedRole) {
+    return <Navigate to="/" replace />;
   }
   return children;
 }
