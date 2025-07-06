623cbf330a65e03befe6793e174f82c742b6840a
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-06 20:44:33 +0545

    feat: add AdminDashboard component for admin-

---
+++ assignment-6/assignment-6/src/components/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Manage your application settings and user accounts here.</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
