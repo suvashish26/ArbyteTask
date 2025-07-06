c217a7658c6c866911d99e53620796a3c7b40f6d
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-06 20:43:18 +0545

    feat: add login form with authentication logic and redirect to admin dashboard on success

--- assignment-6/assignment-6/src/components/Login.jsx
+++ assignment-6/assignment-6/src/components/Login.jsx
@@ -1,11 +1,40 @@
-export default function Login() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+export default function Login({ setIsAuthenticated }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  function handleLogin(e) {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Email and password are required");
+      return;
+    }
+    setIsAuthenticated(true);
+    navigate("/admin");
+  }
   return (
     <div>
-      <h1>Login</h1>
-      <label htmlFor="Email">Email</label>
-      <input type="email" placeholder="Email.." />
-      <label htmlFor="Password">Password</label>
-      <input type="password" placeholder="Password.." />
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <label htmlFor="Email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email.."
+        />
+        <label htmlFor="Password">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password.."
+        />
+        <button type="submit">Login</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
     </div>
   );
 }
