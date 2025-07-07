774506ca3f785b0b0130461aeb5d0baee4f5a100
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-07 17:39:05 +0545

    chore: fixes

--- assignment-7/assignment-7/src/main.jsx
+++ assignment-7/assignment-7/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Tailwind or your CSS setup
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
