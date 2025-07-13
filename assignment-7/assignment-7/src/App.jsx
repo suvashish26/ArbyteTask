2feb2fb1a7b20f1d1ea59ad20dc17efaf1da2d21
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-13 11:41:08 +0545

    latest changes

--- assignment-7/assignment-7/src/App.jsx
+++ assignment-7/assignment-7/src/App.jsx
@@ -1,25 +1,40 @@
-// src/App.jsx
+// // src/App.jsx
+// import React from "react";
+// // import { AuthProvider } from "./context/AuthContext";
+// // import Home from "./pages/Home";
+// import Home from "./components/Home";
+// import NoteState from "./context/Notes/NoteState";
+// import About from "./components/About";
+// import LoginState from "./context/Login/LoginState";
+// import Login from "./components/Login";
+// export default function App() {
+//   return (
+//     <>
+//       <NoteState>
+//         {/* <AuthProvider>
+//           <Home />
+//         </AuthProvider> */}
+//         <About />
+//         <Home />
+//       </NoteState>
+//       <LoginState>
+//         <Login />
+//       </LoginState>
+//     </>
+//   );
+// }
 import React from "react";
-// import { AuthProvider } from "./context/AuthContext";
-// import Home from "./pages/Home";
-import Home from "./components/Home";
-import NoteState from "./context/Notes/NoteState";
-import About from "./components/About";
 import LoginState from "./context/Login/LoginState";
-import Login from "./components/Login";
-export default function App() {
+import Profile from "./components/Profile";
+
+function App() {
   return (
-    <>
-      <NoteState>
-        {/* <AuthProvider>
-          <Home />
-        </AuthProvider> */}
-        <About />
-        <Home />
-      </NoteState>
+    <div>
       <LoginState>
-        <Login />
+        <Profile />
       </LoginState>
-    </>
+    </div>
   );
 }
+
+export default App;
