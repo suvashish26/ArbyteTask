80e40020c7a7acc6d0d997c42606ea5e9573f34f
Suvashish Shrestha <shresthasuvashish@gmail.com>
2025-07-25 13:17:34 +0545

    final commit

--- assignment-7/assignment-7/src/context/ThemeContext.jsx
+++ assignment-7/assignment-7/src/context/ThemeContext.jsx
@@ -1,3 +1,19 @@
-import { createContext } from "react";
+// src/context/ThemeContext.jsx
+import { createContext, useState } from "react";
 
-export const ThemeContext = createContext({});
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+
